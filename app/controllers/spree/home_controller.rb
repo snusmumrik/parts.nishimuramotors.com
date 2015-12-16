5ef248b850019ec596bbb22e2fd36909b266afb5
Hiroyuki Kondo <hombredeestemundo@gmail.com>
2015-12-16 17:02:50 +0900

    add /ranking

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -12,5 +12,10 @@ module Spree
       @ranking = Spree::Product.find_by_sql("select * from spree_products where id in (select c.product_id from spree_orders a left join spree_line_items b on a.id = b.order_id left join spree_variants c on b.variant_id = c.id group by c.product_id order by sum(b.quantity) desc) limit 3")
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
+
+    def ranking
+      @ranking = Spree::Product.find_by_sql("select * from spree_products where id in (select c.product_id from spree_orders a left join spree_line_items b on a.id = b.order_id left join spree_variants c on b.variant_id = c.id group by c.product_id order by sum(b.quantity) desc) limit 20")
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
   mount Spree::Core::Engine, :at => '/'
-          # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/ranking' => 'spree/home#ranking'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
